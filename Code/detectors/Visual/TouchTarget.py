ca3e422ce8dd2181941566e158458328e7a566df
hemkan <hemkano1403@gmail.com>
2024-04-17 14:17:10 -0400

    remove append in conditional

--- Code/detectors/Visual/TouchTarget.py
+++ Code/detectors/Visual/TouchTarget.py
@@ -69,11 +69,10 @@ def checkTouchTarget(screenshot_path, xml_path, min_size=(48, 48)):
 										height = data["compos"][i]['height']
 										width = data["compos"][i]['width']
 										if height < 48 or width < 48:
-											violations += 1
-											interactive_elements.append(elements)
+											violations += 1	
 										else:
 											nonViolations += 1
-											interactive_elements.append(elements)
+										interactive_elements.append(elements)
 
 										#print(violations)
 										#print(nonViolations)
